c6b64d96e816aee08b29521ebc63792cc8e90d6c
Saroj Dahal <isarojdahal@gmail.com>
2024-10-03 14:44:09 +0545

    update: Readme.md

--- src/ui/pages/SessionList.tsx
+++ src/ui/pages/SessionList.tsx
@@ -4,14 +4,22 @@ const Events = () => {
   return (
     <div>
       <h1>Our Past Events</h1>
-      <div className="flex flex-start">
+      <div className="flex flex-wrap w-full">
         {sessionsData.map((session) => (
-          <div key={session.title} className="card">
-            <img src={session.flyer} alt={session.title} />
+          <div
+            key={session.title}
+            className="card shadow h-[450px] w-auto p-10 mb-5"
+          >
+            <img
+              src={session.flyer}
+              alt={session.title}
+              className="h-[250px]"
+            />
             <h2>{session.title}</h2>
             <p>{session.instructor}</p>
             <p>{session.date}</p>
             <p>{session.time}</p>
+            <a href={session?.videoURL}>Watch recorded session</a>
           </div>
         ))}
       </div>
