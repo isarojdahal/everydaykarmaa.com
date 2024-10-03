1e330ea798747fe7b459f3b245dadc42ff744a16
Saroj Dahal <isarojdahal@gmail.com>
2024-10-03 14:15:07 +0545

    update: build dir (vite)

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), viteReact()],
+  build: {
+    outDir: "build",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
