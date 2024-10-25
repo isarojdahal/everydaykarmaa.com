c149b62af95d2d3ced61f6609205a618bf321faa
Ashmin Bhujel <ashminbhujel01@gmail.com>
2024-10-25 22:54:54 +0545

    add: Basic implementation of mobile navbar

--- src/ui/molecules/Navbar.tsx
+++ src/ui/molecules/Navbar.tsx
@@ -1,4 +1,13 @@
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import NavLink, { NavLinkType } from "../atoms/NavLink";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../shadcn/sheet";
+import EKLogo from "@/assets/logos/WHITELOGO_transparent@3x.png";
 
 export interface NavBarPropsType {
   navLinks: NavLinkType[];
@@ -18,10 +27,35 @@ export function DesktopNavbar({ navLinks }: NavBarPropsType) {
 }
 
 // Mobile Navbar
-export function MobileNavbar() {
+export function MobileNavbar({ navLinks }: NavBarPropsType) {
   return (
     <nav className="lg:hidden">
-      <p>Mobile Navbar</p>
+      <Sheet>
+        <SheetTrigger asChild>
+          <HamburgerMenuIcon className="size-8" />
+        </SheetTrigger>
+        <SheetContent side="top">
+          <SheetHeader>
+            <SheetTitle>
+              <img
+                src={EKLogo}
+                alt="EverydayKarma Logo"
+                className="block mx-auto size-40"
+              />
+              <p className="text-white ~text-3xl/6xl font-bold text-center -mt-4">
+                EverydayKarma
+              </p>
+            </SheetTitle>
+          </SheetHeader>
+          <div className="mt-10">
+            <ul className="flex flex-col items-center gap-8 text-white">
+              {navLinks.map((navLink: NavLinkType) => (
+                <NavLink key={navLink.title} navLink={navLink} />
+              ))}
+            </ul>
+          </div>
+        </SheetContent>
+      </Sheet>
     </nav>
   );
 }
