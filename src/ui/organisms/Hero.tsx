850debb1cd6bab2aafd8b245a6856d290ff3a856
Aaditya <aadityathapa977@gmail.com>
2024-11-25 19:41:10 +0545

    Add type support for hero logos

--- src/ui/organisms/Hero.tsx
+++ src/ui/organisms/Hero.tsx
@@ -7,7 +7,16 @@ import {
   REACT_LOGO,
   Sql,
 } from "@/assets/hero-logos";
-const logosArray: any = [Html, Css, Javascript, REACT_LOGO, Sql, Docker];
+import { ReactNode } from "react";
+type logosType = ReactNode;
+const logosArray: logosType[] = [
+  <Html />,
+  <Css />,
+  <Javascript />,
+  <REACT_LOGO />,
+  <Sql />,
+  <Docker />,
+];
 export default function Hero() {
   return (
     <div className="w-full min-h-[800px] sm:min-h-screen relative after:content-[''] after:absolute after:inset-0 after:bg-black/65">
@@ -20,7 +29,7 @@ export default function Hero() {
         <div className="w-full h-full flex flex-col ~gap-6/14 items-center justify-center">
           <div className="flex flex-wrap ~gap-4/14 items-center justify-center">
             {logosArray.map((Logo: any) => {
-              return <Logo />;
+              return <div>{Logo}</div>;
             })}
           </div>
           <h1 className="text-gray-300 ~text-3xl/7xl text-center font-bold">
